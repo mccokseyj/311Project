1f53c09fa38b23519c1a8d45b7d153615f7b39b3
Quambus <alexanderpei6@gmail.com>
2017-12-08 12:54:09 -0500

    Update allANS.v

--- allANS.v
+++ allANS.v
@@ -1,14 +1,14 @@
 module allANS(
-    input [15:0] disps,
+	input [19:0] disps,
     input clk,
     output [6:0] seven_out,
     output reg [3:0] AN
     );
 
 reg [1:0] count = 0;
-reg [3:0] seven_in;
+	reg [4:0] seven_in;
 
-character_to_segment a1(seven_in,seven_out);
+all_to_segment a1(seven_in,seven_out);
 
 always @(posedge clk) begin
 count <= count + 1;
@@ -18,25 +18,25 @@ count <= count + 1;
 	0:
 	begin
 	AN <= 4'b0111;
-	seven_in <= disps[15:12];
+		seven_in <= disps[19:15];
 	end
 	
 	1:
 	begin
 	AN <= 4'b1011;
-	seven_in <= disps[11:8];
+		seven_in <= disps[14:10];
 	end
 	
 	2:
 	begin
 	AN <= 4'b1101;
-	seven_in <= disps[7:4];
+		seven_in <= disps[9:5];
 	end
 	
 	3:
 	begin
 	AN <= 4'b1110;
-	seven_in <= disps[3:0];
+		seven_in <= disps[4:0];
 	end
 	
 	endcase
