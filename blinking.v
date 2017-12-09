c7c59f220146218d2ef9f8cc47034dd75ea91cbe
Quambus <alexanderpei6@gmail.com>
2017-12-09 14:22:58 -0500

    Update blinking.v

--- blinking.v
+++ blinking.v
@@ -19,14 +19,13 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module blinking(
-    input [19:0] disps,
-    input [1:0] blink,
+    //input [22:0] disps,
     input clk,
     output [7:0] seven_out,
     output reg [3:0] AN
     );
 
-//reg [19:0] disps = 20'b00001000100001100100;
+reg [22:0] disps = 23'b00000001000100001100100;
 reg [1:0] count = 0;
 reg [8:0] blinkcount = 0;
 reg [4:0] seven_in;
@@ -40,7 +39,43 @@ always @(posedge new_clk) begin
 blinkcount = blinkcount + 1;
 count <= count + 1;
 
-case(blink)
+case(disps[22])
+0:
+begin
+
+	case(count)
+	
+	0:
+	begin
+	AN <= 4'b0111;
+	seven_in <= disps[19:15];
+	end
+	
+	1:
+	begin
+	AN <= 4'b1011;
+	seven_in <= disps[14:10];
+	end
+	
+	2:
+	begin
+	AN <= 4'b1101;
+	seven_in <= disps[9:5];
+	end
+	
+	3:
+	begin
+	AN <= 4'b1110;
+	seven_in <= disps[4:0];
+	end
+	endcase
+
+end
+
+1:
+begin
+
+case(disps[21:20])
 
 0:
 begin
@@ -297,6 +332,10 @@ case(count)
 	end
 	
 endcase
+end //ending begin
+
+endcase //ending first case
+
 end
 
 endcase
